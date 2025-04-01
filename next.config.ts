400f1ffe4ed0c4038d8e494d1cda0d150b02f2ca
Siddaharth Suman <siddaharthsuman@gmail.com>
2025-04-01 06:32:30 +0000

    setup: configure github actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/ubiquitous-eureka",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
