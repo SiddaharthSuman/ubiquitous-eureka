9c9ad3d111f6bd9863996257aef928102548d34b
Siddaharth Suman <siddaharthsuman@gmail.com>
2025-04-01 09:18:19 +0000

    add sass support

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/ubiquitous-eureka/next.svg"
+          src={`${process.env.NEXT_PUBLIC_BASE_PATH}/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +29,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="/ubiquitous-eureka/vercel.svg"
+              src={`${process.env.NEXT_PUBLIC_BASE_PATH}/vercel.svg`}
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -54,7 +54,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/ubiquitous-eureka/file.svg"
+            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/file.svg`}
             alt="File icon"
             width={16}
             height={16}
@@ -68,7 +68,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/ubiquitous-eureka/window.svg"
+            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/window.svg`}
             alt="Window icon"
             width={16}
             height={16}
@@ -82,7 +82,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/ubiquitous-eureka/globe.svg"
+            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/globe.svg`}
             alt="Globe icon"
             width={16}
             height={16}
